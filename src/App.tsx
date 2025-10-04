258ef069dd06ee1b25fd187253bc70c75cf6fd11
Thanut <thanutketjai@gmail.com>
2025-10-04 14:14:14 +0700

    ปรับให้ textfield รับค่าได้ , call api แสดง qr code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, type ChangeEvent } from "react";
+import TextField from "./components/TextField";
 
-export default function ApiTest() {
+export default function App() {
   const [qrBase64, setQrBase64] = useState<string>("");
-  const data = "https://weather-app-lilac-delta-44.vercel.app/";
+  const [data, setData] = useState<string>("");
+  // const data = "https://weather-app-lilac-delta-44.vercel.app/";
 
   const fetchQr = () => {
+    if (!data) {
+      console.log("ต้องกรอกข้อมูลก่อนสร้าง QR");
+      return;
+    }
     fetch(`https://api.api-ninjas.com/v1/qrcode?format=png&data=${data}`, {
       headers: { "x-api-key": "zWotgNnav2c7qXTFodK3Sg==GRL0AZf710CarL2N" },
     })
       .then((res) => {
+        console.log("Content-Type:", res.headers.get("Content-Type"));
         return res.text();
       })
       .then((base64) => {
@@ -26,26 +33,18 @@ export default function ApiTest() {
 
   return (
     <section className="w-full bg-white rounded-lg p-4">
-      <section className="w-full flex flex-wrap gap-y-4 my-6">
-        <img src={`data:image/png;base64,${qrBase64}`} alt="API Image" />
-
-        {/* <div className="w-full flex justify-end gap-4">
-          <button
-            className={`p-4 px-6 rounded-xl bg-purple-500 text-white border active:scale-95 transition-all ${
-              page > 1 ? "" : "hidden"
-            }`}
-            onClick={() => setPage((prev) => prev - 1)}
-          >
-            Back
-          </button>
-          <button
-            className="p-4 px-6 rounded-xl bg-purple-500 text-white border active:scale-95 transition-all"
-            onClick={() => setPage((prev) => prev + 1)}
-          >
-            Next
-          </button>
-        </div> */}
-      </section>
+      <div className="w-full pad-main">
+        <TextField
+          id="data"
+          name="data"
+          value={data}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setData(e.target.value)}
+        />
+      </div>
+      <button className="w-fit bg-rose-200 p-4 rounded-lg" onClick={fetchQr}>
+        QR Code Generate
+      </button>
+      <img src={`data:image/png;base64,${qrBase64}`} />
     </section>
   );
 }
